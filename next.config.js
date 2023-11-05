74b5bcd0c94c44b914549fe80e76163f0beb312e
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-11-05 03:14:09 -0500

    diable images

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
-  images: {
-    loader: "custom",
-  },
-  transpilePackages: ["next-image-export-optimizer"],
-  env: {
-    nextImageExportOptimizer_imageFolderPath: "images",
-    nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
-    nextImageExportOptimizer_exportFolderName: "optimized-images",
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
-  },
+  // images: {
+  //   loader: "custom",
+  // },
+  // transpilePackages: ["next-image-export-optimizer"],
+  // env: {
+  //   // nextImageExportOptimizer_imageFolderPath: "images",
+  //   // nextImageExportOptimizer_exportFolderPath: "out",
+  //   // nextImageExportOptimizer_quality: 75,
+  //   // nextImageExportOptimizer_storePicturesInWEBP: true,
+  //   // nextImageExportOptimizer_exportFolderName: "optimized-images",
+  //   // nextImageExportOptimizer_generateAndUseBlurImages: true,
+  // },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
