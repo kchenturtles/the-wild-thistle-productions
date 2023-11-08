5e3f684ad085d148112579bdc61bce67b0b36b49
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2023-11-08 10:13:04 -0500

    delete extras

--- pages/shows.js
+++ pages/shows.js
@@ -13,14 +13,6 @@ export const metadata = {
 }; 
 
 export default function Shows ({episodes}) {
-  // console.log("LAKFALKSJF")
-  // console.log(episodes[0]);
-  // console.log("ASIOFHASKJF");
-  // console.log(episodes[1]);
-
-  // const json = JSON.parse(episodes[1].stringify);
-  // console.log(json);
-
   console.log(episodes[0]["description"]);
   
   return (
@@ -72,7 +64,6 @@ export default function Shows ({episodes}) {
   let accessToken = access_token;
   const res = await fetch('https://api.spotify.com/v1/shows/4MqPuRVONKkOiu2SOAYgAf?si=a90b318f18ff4ace', {
 
-  // https://open.spotify.com/show/
     headers: {
       Authorization: 'Bearer ' + accessToken
     }
@@ -90,11 +81,3 @@ export default function Shows ({episodes}) {
   };
 }
 }
-
-
-
-// curl -X POST "https://accounts.spotify.com/api/token" -H "Content-Type: application/x-www-form-urlencoded"  -d "grant_type=client_credentials&client_id=aa9e986bd91742ef913fbf78a66a24ec&client_secret=7749e697db2647fea424bcfd2d8e85ef"
-
-//curl "https://api.spotify.com/show/4MqPuRVONKkOiu2SOAYgAf?si=07bcb65e7de44d0e" -H "Authorization: Bearer  BQBXMedpAk925eX2eSqC4swzZyBDUDSyi5zk-nEa-mAn5q1l3pxGiZT2aNRItNmcJ56lskSfgZLGlpTk4GYhB_HyOEr2hLFBiXO5xEkdiarGrsjjE2g"
-
-// curl "https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb" -H "Authorization: Bearer  BQBXMedpAk925eX2eSqC4swzZyBDUDSyi5zk-nEa-mAn5q1l3pxGiZT2aNRItNmcJ56lskSfgZLGlpTk4GYhB_HyOEr2hLFBiXO5xEkdiarGrsjjE2g"
