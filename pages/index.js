3e25911de05644b14525b9bc7e4471740c0c7f24
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-01-19 00:19:47 -0500

    typing effect

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import { ArrowRight } from "react-feather";
 import fetch from 'node-fetch';
 import React from "react";
 import { InferGetStaticPropsType, GetStaticProps } from 'next'
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Page ({episodes}) {
   return (
@@ -22,9 +23,11 @@ export default function Page ({episodes}) {
       <section className='section container'>
       <Image className={styles.mainImage } src = { mainImage } alt = "Main Photo" />
       <div className = {styles.introText}>
-        <p>
-        Welcome, listener, to the worlds that we have created. In these audio dramas, you will find places where imagination has touched. You can explore the mystery surrounding the Starship Athena in "Lights Out," follow a journey to find love in "Remnants," or struggle to escape the island of Rintheia in "Supernova." Whichever you choose, we hope that you enjoy your visit!
-        </p>
+      <p><TypeAnimation sequence={'Welcome, listener, to the worlds that we have created. In these audio dramas, you will find places where imagination has touched. You can explore the mystery surrounding the Starship Athena in "Lights Out," follow a journey to find love in "Remnants," or struggle to escape the island of Rintheia in "Supernova." Whichever you choose, we hope that you enjoy your visit!'} speed={75}
+        repeat={1}
+        style={{}}/></p>
+        
+        
       </div>
       </section>
       <section className = 'section container'>
