372f747cd4f5b2cbb6e474f8e3317201f6da9cff
kchenturtles <94144671+kchenturtles@users.noreply.github.com>
2024-05-08 18:17:56 -0400

    fix shows page

--- pages/shows.js
+++ pages/shows.js
@@ -3,7 +3,7 @@
 import styles from "./shows.module.css";
 import Image from "next-image-export-optimizer";
 import Link from "next/link";
-import { ExternalLink } from "react-feather";
+import { ExternalLink, Facebook, Mail, PlayCircle } from "react-feather";
 import PageTitle from "../components/page-title";
 import React from "react";
 import fetch from 'node-fetch';
@@ -18,9 +18,31 @@ export const metadata = {
   title: "Shows",
 }; 
 
-var urls = ['https://api.spotify.com/v1/shows/7HdlfouqPBS3PLf1ivCfdN',
+const urls = ['https://api.spotify.com/v1/shows/7HdlfouqPBS3PLf1ivCfdN',
  'https://api.spotify.com/v1/shows/7ePxTS7GYVZ0uBAZrueeaD',
 ];
+
+const actorsLightsOut = new Map();
+actorsLightsOut.set("Sadie Maxwell", "Kate Dickinson");
+actorsLightsOut.set("Zara Williams", "Sally Jamrog");
+actorsLightsOut.set("Miriam Silas", "Katy Silva");
+actorsLightsOut.set("Jo Treman", "Kaeleen Chen");
+actorsLightsOut.set("Worker (Terry)", "Andi Alexander-Smith");
+actorsLightsOut.set("Paul Williams", "Theo Sloan");
+actorsLightsOut.set("Guard", "Alder Chu");
+actorsLightsOut.set("Archive Worker", "Angus Abercrombie");
+actorsLightsOut.set("Passenger", "Jackson Phelps");
+actorsLightsOut.set("Teacher", "Jackson Phelps");
+actorsLightsOut.set("Monster", "Jackson Phelps");
+
+const actorsRemnants = new Map();
+
+const transcriptsLightsOut = "https://drive.google.com/drive/folders/1mf7VR4szJyBBYbRWcl9lWLz7h6rhBpBv"; 
+
+const transcriptsRemnants = "https://drive.google.com/drive/folders/1Eze6Lqetc0Q6aronlB0x7miS7JR35mt-";
+
+const transcripts = [transcriptsLightsOut, transcriptsRemnants];
+const actors = [actorsLightsOut, actorsRemnants];
 
 function setColor(idn, numEpisodes) {
   console.log(idn);
@@ -40,6 +62,11 @@ function setColor(idn, numEpisodes) {
   }
 }
 
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
 
 export default function Shows ({Remnants, LightsOut}) {
   var i = -1;
@@ -49,7 +76,7 @@ export default function Shows ({Remnants, LightsOut}) {
   const [isHighlighted, setHighlighted] = React.useState({id: 0});
   
   return (
-    <section>
+    <section className = "section container">
     <Head>
         <title>Shows | The Wild Thistle Productions</title>
       </Head>
@@ -73,27 +100,40 @@ export default function Shows ({Remnants, LightsOut}) {
         <section className = "section container">     
             <div className = {styles.spacer}></div> 
             <div className = "cols1_1">
-            <Image src = {names[isHighlighted.id].images[0]["url"]} width = "500" height = "500" className = {styles.image}/>
             <div>
               <div className = {styles.showTitle}>{names[isHighlighted.id].name}</div>
+              <hr className = {styles.showUnderline} color="green"/>
               <div className = {styles.releaseDate}><i>Episodes: {names[isHighlighted.id].total_episodes}</i></div>
               <div className = {styles.description}>{names[isHighlighted.id].description.split(".")[0] + "."}</div>
-              <div className = {styles.title}>Releases:</div>
-              <div>{names[isHighlighted.id].episodes.items.map((episode) => {
-                return (<div className = {styles.link} >
-                <Link href = {episode["external_urls"]["spotify"]}><div className = {styles.link}>{episode["name"]}</div></Link>
-                </div>);
-              })}</div>
+              <Link href = {transcripts[isHighlighted.id]} className={styles.link}>All Episode Transcripts</Link>
+              <hr className = {styles.space} color="green"/>
+              <div>Produced by {names[isHighlighted.id].publisher}</div>
+              <hr className = {styles.closeFit} color="green"/>
               <div className = {styles.media}>
               <div className = {styles.mediaTitle}>Share and Support!</div>
               <div className = {styles.icons}>
+              <Link href = "https://www.twitter.com/" target = "_blank"><Twitter className = {styles.mediaIcon}/></Link>
+              <Link href = "https://www.facebook.com/" target = "_blank"><Facebook className = {styles.mediaIcon}/></Link>
+              <Link href = "https://www.gmail.com/" target = "_blank"><Mail className = {styles.mediaIcon}/></Link>
               <Link href = "https://www.instagram.com/the_wild_thistle_productions/" target = "_blank"><Instagram className = {styles.mediaIcon}/></Link>
              <Link href = "https://www.youtube.com/channel/UCh936OgeD_W7DS4bcQv1B0A" target = "_blank"><Youtube className = {styles.mediaIcon}/></Link>
              <Link href = "https://www.buymeacoffee.com/thewildthistleproductions" className = {styles.link} onClick = {() => {setOpen(!isOpen)}}>Buy Me a Coffee</Link>
               </div>
               </div>
-             
-              <div>Produced by {names[isHighlighted.id].publisher}</div>
+              </div>
+              <div>
+              <Image src = {names[isHighlighted.id].images[0]["url"]} width = "500" height = "500" className = {styles.image}/>
+              <div className = {styles.title}>Latest Episode:</div>
+              <div className = {styles.latestEpisodeName}>{names[isHighlighted.id].episodes.items[0]["name"]}</div>
+              <div className={styles.audio}> Preview: <audio controls src={names[isHighlighted.id].episodes.items[0]["audio_preview_url"]} className={styles.preview}>Listen to An Audio Preview Here:</audio></div>
+              <hr className = {styles.space} color="green"/>
+
+              <div>{names[isHighlighted.id].episodes.items.map((episode) => {
+                return (<div><Link href = {episode["external_urls"]["spotify"]}> <div className = {styles.spaceApart}> <div className = {styles.episode}><PlayCircle/>
+                <div class = {styles.episodeName}>{episode["name"]}</div></div> <div>{millisToMinutesAndSeconds(episode["duration_ms"])}</div></div> </Link>
+                    <div color = "plum" className = {styles.episodeUnderline}></div></div>
+                );
+              })}</div>
               </div>
             </div>    
         </section>
