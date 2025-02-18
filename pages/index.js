399e70c211c537e38b86a6a37805e5fdd7ff73ac
Kendree <94144671+kchenturtles@users.noreply.github.com>
2025-02-18 16:55:38 -0500

    text

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Page({ episodes }) {
         <section className='section container'>
           <Image className={styles.mainImage} src={mainImage} alt="Main Photo" />
           <div className={styles.introText}>
-            <p><TypeAnimation sequence={'Welcome, listener, to the worlds that we have created. In these audio dramas, you will find places where imagination has touched. You can explore the mystery surrounding the Starship Athena in "Lights Out," follow a journey to find love in "Remnants," or struggle to escape the island of Rintheia in "Supernova." Whichever you choose, we hope that you enjoy your visit!'} speed={75}
+            <p><TypeAnimation sequence={'Welcome, listener, to the worlds that we have created. In these audio dramas, you will find places where imagination has touched.'} speed={75}
               repeat={1}
               style={{}} /></p>
           </div>
